ae7250b6f4145f70087303ff7432380e8ece078b
Carolina <canetecarolina94@gmail.com>
2023-05-26 01:16:34 -0300

    ejercicio login

--- sitioX/app.js
+++ sitioX/app.js
@@ -5,12 +5,13 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 require('dotenv').config();
-
-const session = require('express-session');
+var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var contactanosRouter = require('./routes/contactanos');
+var loginRouter = require('./routes/admin/login');
+var adminRouter = require('./routes/admin/novedades');
 
 var app = express();
 
@@ -25,68 +26,54 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
-  secret: '1agjjkuuyyhhjkkmjjjjjj',
-  resave: false,
-  saveUninitialized: true
+    secret: '1agjjkuuyyhhjkkmjjjjjj',
+    resave: false,
+    saveUninitialized: true
 }));
 
-require("./ejemplosBD");
-
-app.get('/tarea', function (req, res) {
-  var conocido = Boolean(req.session.nombre);
-  var menor = req.session.edad < 18
-
-  res.render('tarea', {
-    titulo: 'sesiones',
-    conocido: conocido,
-    nombre: req.session.nombre,
-    menor: menor
-  })
-})
-
-app.post('/ingresar', function (req, res) {
-  if (req.body.nombre) {
-    req.session.nombre = req.body.nombre
-  }
-  if (req.body.edad) {
-    req.session.edad = req.body.edad
-  }
-  res.redirect('/tarea');
-})
-
-app.get('/salir', function (req, res) {
-  req.session.destroy();
-  res.redirect('/tarea')
-})
+secured = async (req, res, next) => {
+    try {
+        console.log(req.session.id_usuario);
+        if (req.session.id_usuario) {
+            next();
+        } else {
+            res.redirect('/admin/login')
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/contactanos', contactanosRouter);
+app.use('/admin/login', loginRouter);
+app.use('/admin/novedades', secured, adminRouter);
 
 app.get('/sobre-el-hotel', function (req, res) {
-  res.render("sobre-el-hotel")
+    res.render("sobre-el-hotel")
 })
 
 app.get('/ubicacion', function (req, res) {
-  res.render("ubicacion")
+    res.render("ubicacion")
 })
 
 
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
---
+++ sitioX/models/usuariosModel.js
@@ -0,0 +1,24 @@
+var pool = require('./bd');
+var md5 = require("md5");
+
+const getByUsernameAndPassword = async(user, password) => {
+    try {
+        var query = 'select * from usuarios where usuario = ? and password = ? limit 1';
+        var rows = await pool.query(query, [user, md5(password)]);
+        return rows[0];
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const funcion2 = async(user, password) => {
+    try {
+        var query = 'select * from usuarios where usuario = ? and password = ? limit 1';
+        var rows = await pool.query(query, [user, md5(password)]);
+        return rows[0];
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+module.exports = {getByUsernameAndPassword, funcion2};
